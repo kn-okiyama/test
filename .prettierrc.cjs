c506770175d496de86f62ee4d89b0e3fc0d13dc2
hirahir <hirahiroworks@gmail.com>
2024-07-25 17:43:24 +0900

    グローバル&hirokohayashi

---
+++ .prettierrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-stylus')
+  ],
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+        printWidth: 20000,
+        // 'languageStylus.useSeparator': true,
+        // "languageStylus.useBuiltinFunctions": true,
+      },
+      customSyntax: 'postcss-html',
+    },
+  ],
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // importしたいファイルを記載 
+          additionalData: `@import "src/styles/_mixin.scss", "src/styles/base.scss";`
+        }
+      }
+    }
+  },
+  build: {
+    // 必ずcssを作成
+    inlineStylesheets: 'never',
+  }
+});
